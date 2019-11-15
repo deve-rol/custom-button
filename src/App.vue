abd521b59ce8ee7a8da5e2506562b3712499e80b
Roland <r.simonyan@hybrid.ai>
2019-11-16 00:40:37 +0400

    created

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="wrapper">
+      <custom-button
+              :size="size"
+              :variant="variant"
+              :disabled="disabled"
+      ></custom-button>
+      <div class="settings">
+        <div class="flex-between">
+          <input type="text" v-model="size" placeholder="Enter size">
+          <span class="info" :title="sizeInfo">ⓘ</span>
+        </div>
+        <div class="flex-between">
+          <input type="text" v-model="variant" placeholder="Enter variant">
+          <span class="info" :title="variantInfo">ⓘ</span>
+        </div>
+        <label>
+          <input type="checkbox" v-model="disabled">
+          Disabled Button
+        </label>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import customButton from "./components/customButton";
 
 export default {
   name: 'app',
+  data() {
+    return {
+      size: "",
+      variant: "",
+      disabled: false,
+      sizeInfo: "Enter: lg or sm",
+      variantInfo: "Enter: primary, success, danger, warning or text"
+    }
+  },
   components: {
-    HelloWorld
+    customButton
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+  *{
+    margin: 0;
+    padding: 0;
+    font-family: sans-serif;
+  }
+  .wrapper {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    .settings {
+      margin-top: 50px;
+      display: grid;
+      grid-template-columns: repeat(3, 1fr);
+      grid-gap: 30px;
+
+      .flex-between {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+
+        .info {
+          margin-left: 10px;
+          cursor: pointer;
+          font-weight: bold;
+          color: #1867c0;
+        }
+      }
+    }
+  }
 </style>
---
+++ src/components/customButton.vue
@@ -0,0 +1,76 @@
+<template>
+    <button :class="['button', size, variant]" :disabled="disabled">
+        <slot>Button</slot>
+    </button>
+</template>
+
+<script>
+    export default {
+        name: "customButton",
+        props: {
+            size: {
+                type: String
+            },
+            variant: {
+                type: String
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+.button {
+    border: none;
+    border-radius: 4px;
+    padding: 8px 12px;
+    font-size: 16px;
+    font-weight: bold;
+    color: #ffffff;
+    background-color: #4caf50;
+    cursor: pointer;
+    box-shadow: 0 1px 3px 1px rgba(0, 0, 0, 0.1);
+    transition: all .2s;
+
+    &:hover {
+        box-shadow: 0 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0,0,0,.12);
+    }
+    &.lg {
+        padding: 14px 20px;
+        font-size: 20px;
+    }
+    &.sm {
+        padding: 4px 6px;
+        font-size: 12px;
+    }
+    &.success {
+        background-color: #4caf50;
+    }
+    &.text {
+        background-color: transparent;
+        color: #1867c0;
+        box-shadow: none;
+        &:hover {
+            background-color: #fafafa;
+        }
+    }
+    &.primary {
+        background-color: #1867c0;
+    }
+    &.danger {
+        background-color: #ff5252;
+    }
+    &.warning {
+        background-color: #fb8c00;
+    }
+    &:disabled {
+        background-color: #d6d6d6;
+        color: #777777;
+        box-shadow: none;
+        cursor: default;
+    }
+}
+</style>
